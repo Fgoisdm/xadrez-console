2e9ff52e820c2501186b10e3df682d96362b8fab
Fagner de Gois <fgois.dm@gmail.com>
2018-10-05 13:17:30 -0300

    Comecando a colocar as pecas no tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -7,12 +8,16 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao p;
 
-            p = new Posicao(3, 4);
-
+            //Criando o Tabuleiro do jogo de xadrez
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            //Testando população do rei e torre manualmente
+            tab.colocaPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocaPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocaPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
+            //Imprimindo o Tabuleiro já com as peças
             Tela.imprimirTabuleiro(tab);
 
             Console.ReadLine();
