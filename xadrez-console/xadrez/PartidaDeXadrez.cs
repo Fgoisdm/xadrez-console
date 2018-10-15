5fb38738137b1c3af741cdb20ba78dd90ff16a74
Fagner de Gois <fgois.dm@gmail.com>
2018-10-15 15:51:27 -0300

    adicionando jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -138,6 +138,19 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // #jogadaEspecial promocao
+            if (p is Peao && p.cor == Cor.Branca && destino.linha == 0 || p.cor == Cor.Preta && destino.linha == 7)
+            {
+                p = tab.retirarPeca(destino);
+                pecas.Remove(p);
+                Peca dama = new Dama(tab, p.cor);
+                tab.colocarPeca(dama, destino);
+                pecas.Add(dama);
+            }
+            
+
             if (estaEmCheque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -156,9 +169,7 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
-
+            
             //#jogadaEspecial en passant
             if (p is Peao && destino.linha == origem.linha -2 || destino.linha == origem.linha + 2)
             {
